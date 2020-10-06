b5500539ced52ae1c99dbb639c431734062dc65e
mateja <mateja176@gmail.com>
2020-10-06 11:41:18 +0200

    adds sections

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,21 +5,37 @@ const src = 'http://localhost:4000/150.png';
 
 function App() {
   return (
-    <div className="App">
-      <a href={'/logo192.png'} target="__blank" download={'logo192.png'}>
-        <img src={'/logo192.png'} alt="logo" />
-      </a>
-      <a href={src} target="__blank" download={'150.png'}>
-        <img src={src} crossOrigin="anonymous" alt="150" />
-      </a>
-      <div>
-        <img src={'/logo192.png'} alt="logo" />
-        <button
+    <main style={{ margin: 20 }}>
+      <section>
+        <h2>Same Origin Download</h2>
+        <a href={'/logo192.png'} target="__blank" download={'logo192.png'}>
+          <img src={'/logo192.png'} alt="logo" />
+        </a>
+      </section>
+      <section>
+        <h2>Download Cross Origin Using Anchor</h2>
+        <a href={src} target="__blank" download={'150.png'}>
+          <img src={src} alt="150" />
+        </a>
+        <section>
+          <h3>With Cross Origin Anonymous</h3>
+          <a href={src} target="__blank" download={'150.png'}>
+            <img src={src} crossOrigin="anonymous" alt="150" />
+          </a>
+        </section>
+      </section>
+      <section>
+        <h2>Download Using Canvas</h2>
+        <img
+          src={src}
+          alt="logo"
+          style={{ cursor: 's-resize' }}
           onClick={() => {
             const image = new Image();
 
             image.src = src;
 
+            // * circumvents "Tainted canvas may not be exported error"
             image.crossOrigin = 'anonymous';
 
             image.addEventListener('load', () => {
@@ -41,11 +57,9 @@ function App() {
               a.click();
             });
           }}
-        >
-          Download
-        </button>
-      </div>
-    </div>
+        />
+      </section>
+    </main>
   );
 }
 
